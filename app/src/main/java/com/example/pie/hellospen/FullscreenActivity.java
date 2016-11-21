f1f4d6c680ebdeceb47709b271b86cc29603cdbf
XSTORM\monster <dummy2.kazen@gmail.com>
2016-11-21 14:49:39 +0900

    //**Full Screen

--- app/src/main/java/com/example/pie/hellospen/FullscreenActivity.java
+++ app/src/main/java/com/example/pie/hellospen/FullscreenActivity.java
@@ -129,11 +129,7 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         });
         */
-        mSpenSimpleSurfaceView.setOnClickListener ( new View.OnClickListener () {
-            public void onClick(View view) {
-                toggle ();
-            }
-        } );
+
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
@@ -210,6 +206,21 @@ public class FullscreenActivity extends AppCompatActivity {
         penInfo.color = Color.BLUE;
         mSpenSimpleSurfaceView.setPenSettingInfo ( penInfo );
 
+        //**Full Screen
+        mSpenSimpleSurfaceView.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION );
+
+        /*
+        mSpenSimpleSurfaceView.setOnClickListener ( new View.OnClickListener () {
+            public void onClick(View view) {
+                toggle ();
+            }
+        } );
+        */
     }
 
     @Override
